c23dc4ef3f400625155669f5e0094fa666305489
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2017-12-19 13:51:51 +0200

    luodaan uusi konrtolleri, haetaan tietoja tietokannasta, luodaan näkymään html-taulukkoa asiakkaista

---
+++ MasterDetail/MasterDetail/Controllers/SalesController.cs
@@ -0,0 +1,32 @@
+﻿using MasterDetail.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MasterDetail.Controllers
+{
+    public class SalesController : Controller
+    {
+        // GET: Sales
+        public ActionResult Index()
+        {
+            northwindEntities entities = new northwindEntities();
+            try
+            {
+                //muodostetaan lista asiakkaista
+                List<Customers> model = entities.Customers.ToList();
+                //palautetaan lista näkymänä
+                return View(model);
+            }
+            finally
+            {
+                //resurssien vapautus
+                entities.Dispose();
+            }
+
+            return View();
+        }
+    }
+}
