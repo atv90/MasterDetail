d47a37613a29559ffdda08d22e7a6ee17e12584e
Anne Vaittinen <anne.vaittinen@stud.pointcollege.fi>
2017-12-19 15:27:54 +0200

    taustajärjestelmätoteutus ajaxilla

--- MasterDetail/MasterDetail/Controllers/SalesController.cs
+++ MasterDetail/MasterDetail/Controllers/SalesController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,8 +26,28 @@ namespace MasterDetail.Controllers
                 //resurssien vapautus
                 entities.Dispose();
             }
+        }
 
-            return View();
+        public ActionResult GetOrderData(string id)
+        {
+            northwindEntities entities = new northwindEntities();
+            try
+            {
+                List<Orders> orders = (from o in entities.Orders
+                                       where o.CustomerID == id
+                                       orderby o.OrderDate descending
+                                       select o).ToList();
+                StringBuilder html = new StringBuilder();
+                html.Append("Hello World!");
+
+                var jsonData = new { html = html.ToString() };
+                return Json(jsonData,JsonRequestBehavior.AllowGet);
+            }
+            finally
+            {
+                //resurssien vapautus
+                entities.Dispose();
+            }
         }
     }
 }
